c556d87436fc0b994bb198d3f5c04b7c2d0e200b
Zhavlon <javlonadakhamov@gmail.com>
2022-08-07 19:32:00 +0600

    connect deps and prepared files structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+
+import { CssBaseline, Grid } from "@material-ui/core";
+
 const App = () => {
-	return <div>hi react !</div>;
+	return (
+		<>
+			<CssBaseline />
+			<Header />
+			<Grid container spaceing={3} style={{ width: "100%" }}>
+				<Grid item xs={12} md={4}>
+					<List />
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Map />
+				</Grid>
+			</Grid>
+		</>
+	);
 };
 
 export default App;
