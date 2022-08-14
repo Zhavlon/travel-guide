c7c049beb6bcb76a72f783674b6be9adc18a8eed
Zhavlon <javlonadakhamov@gmail.com>
2022-08-14 21:44:17 +0600

    changed style of Google Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,6 +4,7 @@ import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
 import useStyles from "./styles";
+import { mapStyles } from "./mapStyles";
 
 const Map = ({
 	coordinates,
@@ -18,12 +19,16 @@ const Map = ({
 	return (
 		<div className={classes.mapContainer}>
 			<GoogleMapReact
-				bootstrapURLKeys={{ key: "AIzaSyAF1KLtsArzguJ4yc-K-RRSFw7jZCatB7c" }}
+				bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
 				defaultCenter={coordinates}
 				center={coordinates}
 				defaultZoom={14}
 				margin={[50, 50, 50, 50]}
-				options={""}
+				options={{
+					disableDefaultUI: true,
+					zoomControl: true,
+					styles: mapStyles,
+				}}
 				onChange={e => {
 					setCoordinates({ lat: e.center.lat, lng: e.center.lng });
 					setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
